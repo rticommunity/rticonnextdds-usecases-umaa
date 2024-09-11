1228913f9a6090912dbe13435f08ef0cc450c778
David Greenberg <dgreenberg@rti.com>
2024-09-11 11:23:28 -0400

    update start component script

--- examples/start_component.sh
+++ examples/start_component.sh
@@ -2,51 +2,50 @@
 
 # Add Necessary Files to XML variable
 export NDDS_QOS_PROFILES=""
-
+export PYTHONPATH=""
 exit=false
 
-if [ "$1" == "autopilot" ] || [ "$1" == "usvnav" ]; then
+cmd=""
+
+if [ "$1" == "autopilot" ] || [ "$1" == "usvnav" ] || [ "$1" == "globalvector" ]; then
     component=$1
-    if [ "$3" ]; then
-        export DOMAIN_ID=$3
+    if [ $component == "autopilot" ]; then
+        cmd="./build/$component"
+
+    elif [ "$2" ]; then
+        export DOMAIN_ID=$2
+    else
+        cmd="python3 ./python/$component.py"
+        PYTHONPATH+="$UMAA_TYPES/gen_types/python"
+        echo "PYTHONPATH updated: "
+        echo $PYTHONPATH
     fi
 else
-    $exit=true
-fi
-
-if [ "$2" == "pub" ] || [ "$2" == "sub" ]; then 
-    suffix=$2
-else
-    $exit=true
+    exit=true
 fi
 
 if $exit; then
     echo "pass in: \n
-        arg1: component name: ["autopilot", "usvnav"] \n
-        arg2: pub or sub \n
-        arg3: Domain ID to override components.xml definition 
-        example: start_component.sh autopilot pub 5 "
+        arg1: component name: ["autopilot", "usvnav", "globalvector"] \n
+        arg2: Domain ID to override <components>.xml definition \n
+        example: start_component.sh autopilot 5 "
 else
 
     # These xml files do need to be added in this order:
 
-    # Add Base Components File
-    # XML+="./resources/umaa_6/components/base.xml;"
-
-
     # Add QOS File
-    NDDS_QOS_PROFILES+="./resources/qos/umaa_qos.xml;"
+    NDDS_QOS_PROFILES+="./resources/qos/umaa_qos_lib.xml;"
     
-    # Add UMAA base
-    NDDS_QOS_PROFILES+="./resources/umaa_6/components/base.xml;"
+    # Add UMAA Domain Topics
+    NDDS_QOS_PROFILES+="./resources/umaa_6/domain/umaa_domain_lib.xml;"
     
     # Add desired component
     NDDS_QOS_PROFILES+="./resources/umaa_6/components/${component}.xml"
 
+    echo "NDDS_QOS_PROFILES updated: "
     echo $NDDS_QOS_PROFILES
 
-    # start python script
-    python3 ./python/${component}_${suffix}.py
-
+    # start
+    $cmd
 
 fi
