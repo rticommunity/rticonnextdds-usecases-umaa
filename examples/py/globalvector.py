39450173b3fb8d013b483fb803dbb325ede65dad
David Greenberg <dgreenberg@rti.com>
2024-09-19 20:57:23 -0400

    cleanup/comments on Python scripts

--- examples/py/globalvector.py
+++ examples/py/globalvector.py
@@ -23,6 +23,7 @@ from GlobalVectorExecutionStatusReportType import *
 
 # This example uses Python modules generated from the UMAA IDL files
 
+
 def get_type_name(type):
     # Get full type name from module
     type_str = idl.get_type_support(type).type_name
@@ -33,52 +34,57 @@ def get_type_name(type):
     type_split = (type_str.rsplit('::', 1)[1])
     return type_split
 
-def publisher_main(file):
+
+def publisher_main():
 
     # Need to register all types in component before creating Domain Participant
-    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorCommandType, get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorCommandType))
-    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorCommandAckReportType, get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorCommandAckReportType))
-    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorCommandStatusType, get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorCommandStatusType))
-    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorExecutionStatusReportType, get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorExecutionStatusReportType))
+    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorCommandType,
+                                            get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorCommandType))
+
+    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorCommandAckReportType,
+                                            get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorCommandAckReportType))
+
+    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorCommandStatusType,
+                                            get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorCommandStatusType))
+
+    dds.DomainParticipant.register_idl_type(UMAA_MO_GlobalVectorControl_GlobalVectorExecutionStatusReportType,
+                                            get_type_name(UMAA_MO_GlobalVectorControl_GlobalVectorExecutionStatusReportType))
 
     # All of our XML files are being passed in by the NDDS_QOS_PROFILES env variable
-    qos_provider = dds.QosProvider(file)
+    qos_provider = dds.QosProvider("")
 
-    # Lookup the Participant we defined in our XML file
-    participant = qos_provider.create_participant_from_config("umaa_components_lib::globalvector")
+    # Lookup the Participant we defined in our XML file resources/components/globalvector.xml
+    participant = qos_provider.create_participant_from_config(
+        "umaa_components_lib::globalvector")
 
     # Lookup the Writers
-    globalvector_command_w = dds.DataWriter(participant.find_datawriter("pub::GlobalVectorCommandType"))
+    globalvector_command_w = dds.DataWriter(
+        participant.find_datawriter("pub::GlobalVectorCommandType"))
 
     globalvector_command_sample = UMAA_MO_GlobalVectorControl_GlobalVectorCommandType()
+    
+    # Select the union to send
+    globalvector_command_sample.speed.SpeedRequirementVariantTypeSubtypes.discriminator = UMAA_Common_Speed_SpeedRequirementVariantTypeEnum.WATERSPEEDREQUIREMENTVARIANT_D
+
+    # Set a random value
+    globalvector_command_sample.speed.SpeedRequirementVariantTypeSubtypes.WaterSpeedRequirementVariantVariant.speed.speed = 6
+
+    print(globalvector_command_sample.speed.SpeedRequirementVariantTypeSubtypes)
 
     # write data samples in a loop
     while (True):
-        time.sleep(3)
-
-        # Select the union to send
-        globalvector_command_sample.speed.SpeedRequirementVariantTypeSubtypes.discriminator = UMAA_Common_Speed_SpeedRequirementVariantTypeEnum.WATERSPEEDREQUIREMENTVARIANT_D
-
-        # Set some values
-        globalvector_command_sample.speed.SpeedRequirementVariantTypeSubtypes.WaterSpeedRequirementVariantVariant.speed.speed = 6
-
-        print(globalvector_command_sample.speed.SpeedRequirementVariantTypeSubtypes)
+        time.sleep(5)
 
         globalvector_command_w.write(globalvector_command_sample)
         print("Writing Global Vector Command")
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Global Vector Command Consumer"
     )
-    print("Global Vector Command Consumer")
-
-    parser.add_argument(
-       "-f", "--file", type=str, default="", help="XML Config file"
-    )
+    print("Global Vector Command Consumer \nThis is just an example application to publish a 'Global Vector Command' to the AutoPilot component. \nDoes NOT implement UMAA Flow Control\n\n")
 
     args = parser.parse_args()
 
-    publisher_main(args.file)
+    publisher_main()
--- examples/py/usvnav.py
+++ examples/py/usvnav.py
@@ -19,6 +19,7 @@ from SpeedReportType import *
 from GlobalPoseReportType import *
 from VelocityReportType import *
 
+
 def get_type_name(type):
     # Get full type name from module
     type_str = idl.get_type_support(type).type_name
@@ -29,69 +30,95 @@ def get_type_name(type):
     type_split = (type_str.rsplit('::', 1)[1])
     return type_split
 
+
 def publisher_main():
 
     print("Registering Types")
+    
     # Need to Register all types in Component before creating Domain Participant
-    dds.DomainParticipant.register_idl_type(UMAA_SO_LogReport_LogReportType, get_type_name(UMAA_SO_LogReport_LogReportType))
-    dds.DomainParticipant.register_idl_type(UMAA_SO_HealthReport_HealthReportType, get_type_name(UMAA_SO_HealthReport_HealthReportType))
-    dds.DomainParticipant.register_idl_type(UMAA_SA_GlobalPoseStatus_GlobalPoseReportType, get_type_name(UMAA_SA_GlobalPoseStatus_GlobalPoseReportType))
-    dds.DomainParticipant.register_idl_type(UMAA_SA_VelocityStatus_VelocityReportType, get_type_name(UMAA_SA_VelocityStatus_VelocityReportType))
-    dds.DomainParticipant.register_idl_type(UMAA_SA_SpeedStatus_SpeedReportType, get_type_name(UMAA_SA_SpeedStatus_SpeedReportType))
+    dds.DomainParticipant.register_idl_type(
+        UMAA_SO_LogReport_LogReportType, get_type_name(
+            UMAA_SO_LogReport_LogReportType))
+    
+    dds.DomainParticipant.register_idl_type(
+        UMAA_SO_HealthReport_HealthReportType, get_type_name(
+            UMAA_SO_HealthReport_HealthReportType))
+    
+    dds.DomainParticipant.register_idl_type(
+        UMAA_SA_GlobalPoseStatus_GlobalPoseReportType, get_type_name(
+            UMAA_SA_GlobalPoseStatus_GlobalPoseReportType))
+    
+    dds.DomainParticipant.register_idl_type(
+        UMAA_SA_VelocityStatus_VelocityReportType, get_type_name(
+            UMAA_SA_VelocityStatus_VelocityReportType))
+    
+    dds.DomainParticipant.register_idl_type(
+        UMAA_SA_SpeedStatus_SpeedReportType, get_type_name(
+            UMAA_SA_SpeedStatus_SpeedReportType))
 
     # All of our XML files are being passed in by the NDDS_QOS_PROFILES env variable
     qos_provider = dds.QosProvider("")
 
     print("Creating Participant")
-    # Lookup the Participant we defined in our XML file
-    participant = qos_provider.create_participant_from_config("umaa_components_lib::usvnav")
+    # Lookup the Participant we defined in our XML file resources/components/usvnav.xml
+    participant = qos_provider.create_participant_from_config(
+        "umaa_components_lib::usvnav")
 
     # Lookup the Writers
     print("Looking up writers")
-    speed_report_writer = dds.DataWriter(participant.find_datawriter("pub::SpeedReportType"))
-    globalpose_report_writer = dds.DataWriter(participant.find_datawriter("pub::GlobalPoseReportType"))
-    velocity_report_writer = dds.DataWriter(participant.find_datawriter("pub::VelocityReportType"))
+    speed_report_writer = dds.DataWriter(
+        participant.find_datawriter("pub::SpeedReportType"))
+    
+    globalpose_report_writer = dds.DataWriter(
+        participant.find_datawriter("pub::GlobalPoseReportType"))
+    
+    velocity_report_writer = dds.DataWriter(
+        participant.find_datawriter("pub::VelocityReportType"))
 
+    # Create samples
     speed_report_sample = UMAA_SA_SpeedStatus_SpeedReportType()
     globalpose_report_sample = UMAA_SA_GlobalPoseStatus_GlobalPoseReportType()
     velocity_report_sample = UMAA_SA_VelocityStatus_VelocityReportType()
 
-    speed = 1
-    # write data samples in a loop
+    # Write data samples in a loop
     while (True):
         time.sleep(1)
 
-        # write Speed
-        speed_report_sample.speedThroughWater = speed
+        # Set values
+        speed_report_sample.speedThroughWater = random.randrange(0, 10, 2)
+
+        # Write Speed
         speed_report_writer.write(speed_report_sample)
         print(f'Writing Speed Data: {speed_report_sample.speedThroughWater}')
 
-        speed+=1
-        if (speed > 100):
-            speed = 1
-
-        # write Global Pose
+        # Set values
         globalpose_report_sample.position.geodeticLatitude = 42.361145
         globalpose_report_sample.position.geodeticLongitude = -71.057083
-        globalpose_report_writer.write(globalpose_report_sample)
-        print(f'Writing GlobalPose data Lat: {globalpose_report_sample.position.geodeticLatitude }')
-        print(f'Writing GlobalPose data Long: {globalpose_report_sample.position.geodeticLongitude}')
- 
 
-        # write Velocity
+        # Write Global Pose sample
+        globalpose_report_writer.write(globalpose_report_sample)
+        print(
+            f'Writing GlobalPose data Lat: {globalpose_report_sample.position.geodeticLatitude }')
+        print(
+            f'Writing GlobalPose data Long: {globalpose_report_sample.position.geodeticLongitude}')
+
+        # Set values
         velocity_report_sample.velocity.eastSpeed = random.randrange(0, 20, 2)
         velocity_report_sample.velocity.northSpeed = random.randrange(0, 20, 2)
-        velocity_report_writer.write(velocity_report_sample)
-        print(f'Writing Velocity data eastSpeed: {velocity_report_sample.velocity.eastSpeed}')
-        print(f'Writing Velocity data northSpeed: {velocity_report_sample.velocity.northSpeed}')
 
+        # Write Velocity sample
+        velocity_report_writer.write(velocity_report_sample)
+        print(
+            f'Writing Velocity data eastSpeed: {velocity_report_sample.velocity.eastSpeed}')
+        print(
+            f'Writing Velocity data northSpeed: {velocity_report_sample.velocity.northSpeed}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="UMAA USV NAV Component"
     )
-    print("UMAA USV NAV Component")
+    print("UMAA USV NAV Component. \nReference example that Publishes the necessary topics to be consumed by the AutoPilot component.\n\n")
 
     args = parser.parse_args()
 
