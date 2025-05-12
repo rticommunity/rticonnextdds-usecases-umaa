c6f1d9794c1783c334f93fd9a4ff9cc2b8548d33
David Greenberg <dgreenberg@rti.com>
2025-05-12 12:06:28 -0400

    comment out log to file

--- examples/c++11/autopilot.cxx
+++ examples/c++11/autopilot.cxx
@@ -114,7 +114,7 @@ int main(int argc, char *argv[])
     rti::config::Logger::instance().verbosity(arguments.verbosity);
 
     //Set the debug output to a specific file
-    rti::config::Logger::instance().output_file("debug_output.log");
+    // rti::config::Logger::instance().output_file("debug_output.log");
 
     // Log a debug message to verify - Needs to be at Level 5 Verbosity
     rti::config::Logger::instance().debug("This is a debug message logged to the file.");
